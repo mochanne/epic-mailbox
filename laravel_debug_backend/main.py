66eca458f8ac6f122927686acc48acfc365ac853
Demian Danhof <demiandanhof@gmail.com>
2022-03-13 16:18:43 +0100

    Python file toegevoegd die de arduino uit leest en de database update als er post is.

---
+++ laravel_debug_backend/main.py
@@ -0,0 +1,34 @@
+import mysql.connector
+import time
+
+import serial
+import os
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="laravel_user",
+    passwd="wachtwoord",
+    database="mail_database"
+)
+
+port = serial.Serial("COM3", baudrate=9600, timeout=3.0)
+
+
+mycursor = mydb.cursor()
+while True:
+    rcv = port.readline().strip()
+    ls = [char for char in rcv]
+    print(rcv)
+    if len(ls) == 1:
+        print(ls[0])
+        if(ls[0] == 98):
+            print("post")
+            mycursor.execute("UPDATE letters SET Brief_in_bus = 1;")
+        elif(ls[0] == 97):
+            print("geen post")
+            mycursor.execute("UPDATE letters SET Brief_in_bus = 0;")
+
+    time.sleep(1)
+    mydb.commit()
+
+mydb.close()
