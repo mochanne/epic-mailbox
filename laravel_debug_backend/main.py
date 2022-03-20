65ae21de4b7556609c0a1a4ad7f3f708568a3f70
Demian Danhof <demiandanhof@gmail.com>
2022-03-20 14:32:39 +0100

    Migration voor de geschiedenis tabel toegevoegd. Deze werkt nu met het prototype. Er wordt een entry toegevoegd in de history tabel als er iets op de sensor wordt gelegd.

--- laravel_debug_backend/main.py
+++ laravel_debug_backend/main.py
@@ -19,13 +19,21 @@ while True:
     rcv = port.readline().strip()
     ls = [char for char in rcv]
     print(rcv)
-    if len(ls) == 1:
 
+    f = open("state.txt", "r")
+    current_state = f.read()
+    f.close()
+
+    if len(ls) == 1:
+    
         if(ls[0] == 98):
             print("post")
             mycursor.execute("UPDATE letters SET Brief_in_bus = 1;")
             f = open("state.txt", "w")
             f.write("1")
+            f.close()
+            f = open("state.txt", "r")
+            new_state = f.read()
             f.close()
         elif(ls[0] == 97):
             print("geen post")
@@ -33,6 +41,12 @@ while True:
             f = open("state.txt", "w")
             f.write("0")
             f.close()
+            f = open("state.txt", "r")
+            new_state = f.read()
+            f.close()
+        if new_state != current_state:
+            if new_state == "1":
+                mycursor.execute("INSERT INTO history VALUES (id, 'brief', CURRENT_TIMESTAMP)")
 
     mydb.commit()
 
